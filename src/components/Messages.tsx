ace271e23b2a93df555039e48da197d37fd18f0b
dczysz <32844299+dczysz@users.noreply.github.com>
2020-04-01 19:13:37 -0600

    Scroll to bottom on new message

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { StyledMessages } from './styles';
 import Message from './Message';
@@ -10,11 +10,27 @@ interface Props {
 }
 
 const Messages: React.FC<Props> = ({ messages, name }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null!);
+
+  // Scroll to bottom on new message
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      //TODO: show 'new messages' banner if not already at bottom
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <StyledMessages>
       {messages.map((msg, i) => (
         <Message key={i} name={name} message={msg} />
       ))}
+      <div ref={messagesEndRef} />
+      {<div className="bottom-banner">{}</div>}
     </StyledMessages>
   );
 };
